aea3d8077307eac676487086328b16838b8e0f2f
FlorianButz <fb8599422@gmail.com>
2024-09-07 14:58:29 +0200

    Fix HardwareMonitor

--- DynamicWin/Utils/HardwareMonitor.cs
+++ DynamicWin/Utils/HardwareMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using LibreHardwareMonitor.Hardware;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace DynamicWin.Utils
             timer.Interval = 1000;
             timer.Elapsed += Timer_Elapsed;
 
+            computer = new Computer()
+            {
+                IsMemoryEnabled = true,
+                IsCpuEnabled = true // Enable CPU monitoring
+            };
+
             timer.Start();
         }
 
@@ -30,56 +37,80 @@ namespace DynamicWin.Utils
         float lastCpu = 0;
         string lastRam = "";
 
+        private static long _iPrevCall;
+        private static bool _isBusy;
         private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
-            computer = new Computer()
+            if (_isBusy)
             {
-                IsMemoryEnabled = true,
-                IsCpuEnabled = true // Enable CPU monitoring
-            };
-            computer.Open();
-         
-            foreach (var hardware in computer.Hardware)
-            {
-                if (hardware.HardwareType == HardwareType.Cpu)
-                {
-                    if (hardware == null) continue;
-                    hardware.Update();
-                    foreach (var sensor in hardware.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Load && sensor.Name == "CPU Total")
-                        {
-                            lastCpu = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
-                        }
-                    }
-                }
-
-                if (hardware.HardwareType == HardwareType.Memory)
-                {
-                    if (hardware == null) continue;
-                    hardware.Update();
-
-                    float memUsed = 0;
-                    float memFree = 0;
-
-                    foreach (var sensor in hardware.Sensors)
-                    {
-                        if (sensor.Name == "Memory Used")
-                        {
-                            memUsed = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
-                        }
-                        else if (sensor.Name == "Memory Available")
-                        {
-                            memFree = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
-                        }
-                        lastRam = memUsed + "GB / " + Mathf.LimitDecimalPoints(memFree + memUsed, 0) + "GB";
-                    }
-                }
+                // Skip this time.
+                Debug.WriteLine($"HardwareMonitor SKIPPED when called again during #{_iPrevCall}");
+                return;
             }
 
-            usageString = $"CPU: {lastCpu}%    RAM: {lastRam}";
+            _isBusy = true;
+            _iPrevCall++;
+            var iCall = _iPrevCall;
+            try
+            {
+                Debug.WriteLine($"HardwareMonitor BEFORE #{iCall}");
 
-            instance.computer.Close();
+                computer.Open();
+
+                foreach (var hardware in computer.Hardware)
+                {
+                    if (hardware.HardwareType == HardwareType.Cpu)
+                    {
+                        if (hardware == null) continue;
+                        hardware.Update();
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Load && sensor.Name == "CPU Total")
+                            {
+                                lastCpu = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
+                            }
+                        }
+                    }
+
+                    if (hardware.HardwareType == HardwareType.Memory)
+                    {
+                        if (hardware == null) continue;
+                        hardware.Update();
+
+                        float memUsed = 0;
+                        float memFree = 0;
+
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.Name == "Memory Used")
+                            {
+                                memUsed = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
+                            }
+                            else if (sensor.Name == "Memory Available")
+                            {
+                                memFree = Mathf.LimitDecimalPoints((float)sensor.Value.GetValueOrDefault(), 1);
+                            }
+                            lastRam = memUsed + "GB / " + Mathf.LimitDecimalPoints(memFree + memUsed, 0) + "GB";
+                        }
+                    }
+                }
+
+                usageString = $"CPU: {lastCpu}%    RAM: {lastRam}";
+
+                instance.computer.Close();
+
+                Debug.WriteLine($"HardwareMonitor AFTER #{iCall}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                Debug.WriteLine($"HardwareMonitor EXCEPTION #{iCall}");
+                return;
+            }
+            finally
+            {
+                _isBusy = false;
+            }
         }
 
         public static void Stop()
