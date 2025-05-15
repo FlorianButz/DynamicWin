ebd15fd241a7984c4afc756eda07a84bf43e13e4
Megan <coeur@ceremonyofsin.org>
2025-05-14 22:02:17 -0600

    Move multi-selection button logic out of SettingsMenu for flexibility, refactoring

--- DynamicWin/UI/Menu/Menus/SettingsMenu.cs
+++ DynamicWin/UI/Menu/Menus/SettingsMenu.cs
@@ -92,7 +92,7 @@ namespace DynamicWin.UI.Menu.Menus
                 objects.Add(islandModesTitle);
 
                 var islandModes = new string[] { "Island", "Notch" };
-                var islandMode = new MultiSelectionButton(island, islandModes, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
+                var islandMode = new DWMultiSelectionButton(island, islandModes, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
                 islandMode.SelectedIndex = (Settings.IslandMode == IslandObject.IslandMode.Island) ? 0 : 1;
                 islandMode.Anchor.X = 0;
                 islandMode.onClick += (index) =>
@@ -137,7 +137,7 @@ namespace DynamicWin.UI.Menu.Menus
                     else selectedMonitors[i] = "Monitor " + i;
                 }
 
-                var selectedMonitor = new MultiSelectionButton(island, selectedMonitors, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
+                var selectedMonitor = new DWMultiSelectionButton(island, selectedMonitors, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
                 selectedMonitor.SelectedIndex = Math.Clamp(Settings.ScreenIndex, 0, MainForm.GetMonitorCount() - 1);
                 selectedMonitor.Anchor.X = 0;
                 selectedMonitor.onClick += (index) =>
@@ -156,7 +156,7 @@ namespace DynamicWin.UI.Menu.Menus
                 objects.Add(themeTitle);
 
                 var themeOptions = new string[] { "Custom", "Dark", "Light", "Candy", "Forest Dawn", "Sunset Glow" };
-                var theme = new MultiSelectionButton(island, themeOptions, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
+                var theme = new DWMultiSelectionButton(island, themeOptions, new Vec2(25, 0), new Vec2(IslandSize().X - 50, 25), UIAlignment.TopLeft);
                 theme.SelectedIndex = Settings.Theme + 1;
                 theme.Anchor.X = 0;
                 theme.onClick += (index) =>
@@ -286,8 +286,9 @@ namespace DynamicWin.UI.Menu.Menus
             };
             backBtn.Text.Font = Resources.Res.InterBold;
 
-            bottomMask = new UIObject(island, Vec2.zero, new Vec2(IslandSizeBig().X - 225, 175), UIAlignment.BottomCenter)
+            bottomMask = new UIObject(island, Vec2.zero, new Vec2(IslandSizeBig().X - 230, 75), UIAlignment.BottomCenter)
             {
+                Anchor = new Vec2(0.5, 1.1),
                 Color = Theme.IslandBackground,
                 roundRadius = 50
             };
@@ -942,77 +943,6 @@ namespace DynamicWin.UI.Menu.Menus
         {
             IsChecked = !IsChecked;
             base.OnMouseUp();
-        }
-    }
-
-    public class MultiSelectionButton : UIObject
-    {
-        string[] options;
-        DWTextButton[] buttons;
-
-        public Action<int> onClick;
-
-        int selectedIndex = 0;
-        public int SelectedIndex { get => selectedIndex; set => SetSelected(value); }
-
-        public MultiSelectionButton(UIObject? parent, string[] options, Vec2 position, Vec2 size, UIAlignment alignment = UIAlignment.TopCenter, int maxInOneRow = 4) : base(parent, position, size, alignment)
-        {
-            this.options = options;
-            this.buttons = new DWTextButton[options.Length];
-
-            float xPos = 0;
-            float yPos = 0;
-            int counter = 0;
-
-            for(int i = 0; i < options.Length; i++)
-            {
-                var lambdaIndex = i; // Either I'm going insane or I don't understand lambdas, but it seems like only the pointer given in to the OnClick() method. This is why this line is needed!
-                var action = () => { OnClick(lambdaIndex); }; // For some it just outputs the length of options if there is no seperate variable for it.
-
-                if (counter >= maxInOneRow)
-                {
-                    counter = 0;
-                    yPos += 35;
-                    xPos = 0;
-                }
-
-                var btn = new DWTextButton(this, options[i], new Vec2(xPos, yPos), new Vec2(Math.Max(75, options[i].Length >= 11 ? (options[i].Length * 9) : 0), 25), action, UIAlignment.MiddleLeft);
-                btn.Text.Color = Theme.TextSecond;
-                btn.Anchor.X = 0;
-                buttons[i] = btn;
-                
-                AddLocalObject(btn);
-
-                xPos += btn.Size.X + 15;
-                counter++;
-            }
-
-            Size.Y = Size.Y + yPos;
-
-            SelectedIndex = 0;
-        }
-
-        public override void Draw(SKCanvas canvas)
-        {
-        }
-
-        void SetSelected(int index)
-        {
-            selectedIndex = index;
-
-            foreach (var button in buttons)
-            {
-                button.normalColor = Theme.Secondary.Override(a: 0.9f);
-            }
-
-            buttons[index].normalColor = (Theme.Primary * 0.65f).Override(a: 0.75f);
-        }
-
-        void OnClick(int index)
-        {
-            SelectedIndex = index;
-
-            onClick.Invoke(index);
         }
     }
 }
